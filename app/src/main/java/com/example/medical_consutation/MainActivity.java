7c6dc297af6e250d8635d3ba4a83f5d9b15d3c00
mwangangi1998 <mwangangimusyoka1998@gmail.com>
2022-06-04 04:12:43 +0300

    activities

---
+++ app/src/main/java/com/example/medical_consutation/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.medical_consutation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+Button btn1,btn2;
+TextView tv1,tv2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setTheme(R.style.Theme_Medicalconsutation);
+        setContentView(R.layout.activity_main);
+        btn1 =(Button) findViewById(R.id.createaccount);
+        btn2 =(Button) findViewById(R.id.signin);
+        tv1 =(TextView) findViewById(R.id.caption);
+        tv2=(TextView) findViewById(R.id.logo);
+        tv1.setText("Your health your mate");
+        tv2.setText("Medic APP");
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openActivities();
+            }
+        });
+
+    }
+
+    private void openActivities() {
+        Intent intent =new Intent(this,createAccount.class);
+        startActivity(intent);
+    }
+}
---
+++ app/src/main/java/com/example/medical_consutation/createAccount.java
@@ -0,0 +1,14 @@
+package com.example.medical_consutation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class createAccount extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_account);
+    }
+}
